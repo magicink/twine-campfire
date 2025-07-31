9fa265caa5846624e098a3bb69a2dcc22f9c1370
Brandon Tom <magicink@gmail.com>
2025-07-31 05:49:33 -0700

    fix(apps/campfire): update root element ID in campfire main file

--- apps/campfire/src/main.tsx
+++ apps/campfire/src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Story } from './Story'
 
-const root = document.getElementById('root')
+const root = document.getElementById('story-root')
 if (root) {
   createRoot(root).render(<Story />)
 }
