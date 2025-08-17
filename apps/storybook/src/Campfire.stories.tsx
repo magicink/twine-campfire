b8429c8a4a64f1049888004aea3c891acfdbba62
Brandon Tom <magicink@gmail.com>
2025-08-16 18:21:55 -0700

    feat(campfire): update Campfire stories to include Deck directives

--- apps/storybook/src/Campfire.stories.tsx
+++ apps/storybook/src/Campfire.stories.tsx
@@ -2,23 +2,17 @@ import type { Meta, StoryObj } from '@storybook/preact'
 import { Campfire } from '@campfire/components'
 
 const meta: Meta = {
-  title: 'Campfire'
+  title: 'Campfire/Directives'
 }
 
 export default meta
 
 // A super-minimal story that renders TwineJS custom elements directly in JSX.
 // This validates our JSX and DOM typings for these tags.
-export const Triggers: StoryObj = {
+export const Trigger: StoryObj = {
   render: () => (
     <>
-      <tw-storydata
-        name='Storybook Demo'
-        startnode='1'
-        data-demo={true}
-        tags='demo'
-        options='debug'
-      >
+      <tw-storydata startnode='1' options='debug'>
         <tw-passagedata pid='1' name='Start'>
           {`
 :set[test=true]
@@ -41,16 +35,22 @@ You clicked the button!
     </>
   )
 }
+
+// Expected component composition of the Deck directives below (JSX-style):
+// <Deck size={{ width: 800, height: 600 }}>
+//   <Slide transition={{ type: 'fade' }}>
+//     <Appear at={0}>
+//       <DeckText as="h2" x={80} y={80}>Hello</DeckText>
+//     </Appear>
+//     <Appear at={1}>
+//       <DeckText as="h2" x={100} y={100}>World</DeckText>
+//     </Appear>
+//   </Slide>
+// </Deck>
 export const Deck: StoryObj = {
   render: () => (
     <>
-      <tw-storydata
-        name='Storybook Demo'
-        startnode='1'
-        data-demo={true}
-        tags='demo'
-        options='debug'
-      >
+      <tw-storydata startnode='1' options='debug'>
         <tw-passagedata pid='1' name='Start'>
           {`
 :::deck{size=800x600}
