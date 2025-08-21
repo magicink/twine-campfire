b6bcd55984d020e9bab70e5865ed207f21c73d35
Brandon Tom <140762401+signalcafe@users.noreply.github.com>
2025-08-20 18:46:23 -0700

    feat: add for directive story

---
+++ apps/storybook/src/ForDirective.stories.tsx
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from '@storybook/preact'
+import { Campfire } from '@campfire/components'
+
+const meta: Meta = {
+  title: 'Campfire/Directives/For'
+}
+
+export default meta
+
+/**
+ * Iterates over an array of fruits using the `for` directive and skips
+ * bananas via a nested `if` directive.
+ *
+ * @returns Campfire story demonstrating the `for` directive.
+ */
+export const Fruits: StoryObj = {
+  render: () => (
+    <>
+      <tw-storydata startnode='1' options='debug'>
+        <tw-passagedata pid='1' name='Start'>
+          {`
+:array[fruits=["apple","banana","cherry"]]
+
+:::for[fruit in fruits]
+
+  :::if{fruit !== "banana"}
+
+  - :show[fruit]
+
+  :::
+
+:::
+          `}
+        </tw-passagedata>
+      </tw-storydata>
+      <Campfire />
+    </>
+  )
+}
