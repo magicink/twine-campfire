ae381a84af1212de40a01718845deb852370029d
Brandon Tom <140762401+signalcafe@users.noreply.github.com>
2025-08-21 21:31:56 -0700

    chore(storybook): control text size and width

--- apps/storybook/src/DeckDirectiveMultiPassage.stories.tsx
+++ apps/storybook/src/DeckDirectiveMultiPassage.stories.tsx
@@ -7,12 +7,20 @@ import { Campfire } from '@campfire/components'
 interface MultiPassageArgs {
   firstDeckFirstX: number
   firstDeckFirstY: number
+  firstDeckFirstW: number
+  firstDeckFirstSize: number
   firstDeckSecondX: number
   firstDeckSecondY: number
+  firstDeckSecondW: number
+  firstDeckSecondSize: number
   secondDeckFirstX: number
   secondDeckFirstY: number
+  secondDeckFirstW: number
+  secondDeckFirstSize: number
   secondDeckSecondX: number
   secondDeckSecondY: number
+  secondDeckSecondW: number
+  secondDeckSecondSize: number
 }
 
 const meta: Meta<MultiPassageArgs> = {
@@ -20,22 +28,38 @@ const meta: Meta<MultiPassageArgs> = {
   argTypes: {
     firstDeckFirstX: { control: { type: 'number' } },
     firstDeckFirstY: { control: { type: 'number' } },
+    firstDeckFirstW: { control: { type: 'number' } },
+    firstDeckFirstSize: { control: { type: 'number' } },
     firstDeckSecondX: { control: { type: 'number' } },
     firstDeckSecondY: { control: { type: 'number' } },
+    firstDeckSecondW: { control: { type: 'number' } },
+    firstDeckSecondSize: { control: { type: 'number' } },
     secondDeckFirstX: { control: { type: 'number' } },
     secondDeckFirstY: { control: { type: 'number' } },
+    secondDeckFirstW: { control: { type: 'number' } },
+    secondDeckFirstSize: { control: { type: 'number' } },
     secondDeckSecondX: { control: { type: 'number' } },
-    secondDeckSecondY: { control: { type: 'number' } }
+    secondDeckSecondY: { control: { type: 'number' } },
+    secondDeckSecondW: { control: { type: 'number' } },
+    secondDeckSecondSize: { control: { type: 'number' } }
   },
   args: {
     firstDeckFirstX: 20,
     firstDeckFirstY: 20,
+    firstDeckFirstW: 100,
+    firstDeckFirstSize: 24,
     firstDeckSecondX: 20,
     firstDeckSecondY: 60,
+    firstDeckSecondW: 100,
+    firstDeckSecondSize: 24,
     secondDeckFirstX: 20,
     secondDeckFirstY: 20,
+    secondDeckFirstW: 100,
+    secondDeckFirstSize: 24,
     secondDeckSecondX: 20,
-    secondDeckSecondY: 40
+    secondDeckSecondY: 40,
+    secondDeckSecondW: 100,
+    secondDeckSecondSize: 24
   }
 }
 
@@ -50,12 +74,20 @@ export const MultiPassage: StoryObj<MultiPassageArgs> = {
   render: ({
     firstDeckFirstX,
     firstDeckFirstY,
+    firstDeckFirstW,
+    firstDeckFirstSize,
     firstDeckSecondX,
     firstDeckSecondY,
+    firstDeckSecondW,
+    firstDeckSecondSize,
     secondDeckFirstX,
     secondDeckFirstY,
+    secondDeckFirstW,
+    secondDeckFirstSize,
     secondDeckSecondX,
-    secondDeckSecondY
+    secondDeckSecondY,
+    secondDeckSecondW,
+    secondDeckSecondSize
   }) => (
     <>
       <tw-storydata startnode='1' options='debug'>
@@ -67,13 +99,13 @@ export const MultiPassage: StoryObj<MultiPassageArgs> = {
     [[Next->Second]]
 
     :::reveal{at=0}
-      :::text{x=${firstDeckFirstX} y=${firstDeckFirstY}}
+      :::text{x=${firstDeckFirstX} y=${firstDeckFirstY} w=${firstDeckFirstW} size=${firstDeckFirstSize}}
       First deck 1
       :::
     :::
 
     :::reveal{at=1}
-      :::text{x=${firstDeckSecondX} y=${firstDeckSecondY}}
+      :::text{x=${firstDeckSecondX} y=${firstDeckSecondY} w=${firstDeckSecondW} size=${firstDeckSecondSize}}
       First deck 2
       :::
     :::
@@ -89,13 +121,13 @@ export const MultiPassage: StoryObj<MultiPassageArgs> = {
     [[Back->Start]]
 
     :::reveal{at=0}
-      :::text{x=${secondDeckFirstX} y=${secondDeckFirstY}}
+      :::text{x=${secondDeckFirstX} y=${secondDeckFirstY} w=${secondDeckFirstW} size=${secondDeckFirstSize}}
       Second deck 1
       :::
     :::
 
     :::reveal{at=1}
-      :::text{x=${secondDeckSecondX} y=${secondDeckSecondY}}
+      :::text{x=${secondDeckSecondX} y=${secondDeckSecondY} w=${secondDeckSecondW} size=${secondDeckSecondSize}}
       Second deck 2
       :::
     :::
@@ -104,7 +136,7 @@ export const MultiPassage: StoryObj<MultiPassageArgs> = {
 `}
         </tw-passagedata>
       </tw-storydata>
-      <Campfire />
+      <Campfire data-testid='campfire' />
     </>
   )
 }
