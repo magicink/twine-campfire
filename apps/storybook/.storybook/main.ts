834e3f59f14e6cd1417230e3613ee7fa8dbed08c
Brandon Tom <magicink@gmail.com>
2025-08-28 09:36:32 -0700

    feat: add storybook a11y checks

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -3,6 +3,7 @@ import type { StorybookConfig } from '@storybook/preact-vite'
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(ts|tsx)'],
+  addons: ['@storybook/addon-a11y'],
   framework: {
     name: '@storybook/preact-vite',
     options: {}
