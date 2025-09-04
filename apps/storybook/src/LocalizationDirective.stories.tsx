de722977cd6e16492d6f6392282d27c7920993e9
Brandon Tom <magicink@gmail.com>
2025-09-03 18:24:04 -0700

    docs: add localization select story

---
+++ apps/storybook/src/LocalizationDirective.stories.tsx
@@ -0,0 +1,49 @@
+import type { Meta, StoryObj } from '@storybook/preact'
+import { Campfire } from '@campfire/components'
+
+const meta: Meta = {
+  title: 'Campfire/Directives/Localization'
+}
+
+export default meta
+
+/**
+ * Builds a language selector using localization directives.
+ *
+ * @returns Campfire story showcasing a language select menu.
+ */
+export const LanguageSelect: StoryObj = {
+  render: () => (
+    <>
+      <tw-storydata startnode='1' options='debug'>
+        <tw-passagedata pid='1' name='Start'>
+          {`
+::set[lang="en-US"]
+::setLanguageLabel[en-US="English (US)"]
+::setLanguageLabel[fr="Français"]
+::setLanguageLabel[th="ไทย"]
+
+::translations[en-US]{ui:greet="Hello"}
+::translations[fr]{ui:greet="Bonjour"}
+::translations[th]{ui:greet="สวัสดี"}
+
+::set[languages=getLanguages()]
+
+::lang[lang]
+
+:::select[lang]{label="Choose a language" data-testid="language-select"}
+:::for[l in languages]
+:::option{value=l.code}
+:show[l.label]
+:::
+:::
+:::
+
+:t[ui:greet]
+          `}
+        </tw-passagedata>
+      </tw-storydata>
+      <Campfire />
+    </>
+  )
+}
