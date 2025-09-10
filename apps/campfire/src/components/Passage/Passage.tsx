047d8dc33148b52c77693e57dc9d9af7653f2322
Brandon Tom <magicink@gmail.com>
2025-09-10 13:47:55 -0700

    refactor(passage): add directive component map module (#708)

--- apps/campfire/src/components/Passage/Passage.tsx
+++ apps/campfire/src/components/Passage/Passage.tsx
@@ -16,29 +16,7 @@ import {
   type StoryDataState
 } from '@campfire/state/useStoryDataStore'
 import { useDeckStore } from '@campfire/state/useDeckStore'
-import { LinkButton } from '@campfire/components/Passage/LinkButton'
-import { TriggerButton } from '@campfire/components/Passage/TriggerButton'
-import { Input } from '@campfire/components/Passage/Input'
-import { Checkbox } from '@campfire/components/Passage/Checkbox'
-import { Radio } from '@campfire/components/Passage/Radio'
-import { Textarea } from '@campfire/components/Passage/Textarea'
-import { Select } from '@campfire/components/Passage/Select'
-import { Option } from '@campfire/components/Passage/Option'
-import { If } from '@campfire/components/Passage/If'
-import { Show } from '@campfire/components/Passage/Show'
-import { Translate } from '@campfire/components/Passage/Translate'
-import { OnExit } from '@campfire/components/Passage/OnExit'
-import { Effect } from '@campfire/components/Passage/Effect'
-import { Deck } from '@campfire/components/Deck/Deck'
-import {
-  Slide,
-  SlideReveal,
-  SlideText,
-  SlideImage,
-  SlideShape,
-  SlideEmbed,
-  Layer
-} from '@campfire/components/Deck/Slide'
+import { componentMap } from '@campfire/components/Passage/componentMap'
 
 const DIRECTIVE_MARKER_PATTERN = '(:::[^\\n]*|:[^\\n]*|<<)'
 
@@ -84,33 +62,10 @@ export const Passage = () => {
   const handlers = useDirectiveHandlers()
   const processor = useMemo(
     () =>
-      createMarkdownProcessor(
-        handlers,
-        {
-          button: LinkButton,
-          trigger: TriggerButton,
-          input: Input,
-          checkbox: Checkbox,
-          radio: Radio,
-          textarea: Textarea,
-          select: Select,
-          option: Option,
-          if: If,
-          show: Show,
-          translate: Translate,
-          effect: Effect,
-          onExit: OnExit,
-          deck: Deck,
-          slide: Slide,
-          reveal: SlideReveal,
-          layer: Layer,
-          slideText: SlideText,
-          slideImage: SlideImage,
-          slideShape: SlideShape,
-          slideEmbed: SlideEmbed
-        },
-        [remarkParagraphStyles, remarkHeadingStyles]
-      ),
+      createMarkdownProcessor(handlers, componentMap, [
+        remarkParagraphStyles,
+        remarkHeadingStyles
+      ]),
     [handlers]
   )
   const passage = useStoryDataStore((state: StoryDataState) =>
